cce09cd4b8ff5184ecf48cd26977f351f48fcce5
sagar <96648429+sagar285@users.noreply.github.com>
2024-08-27 23:04:15 +0530

    indexing part 2

--- route4.js
+++ route4.js
@@ -20,8 +20,7 @@ Router.get("/indexing", async (req, res) => {
 });
 Router.get("/indexingstats", async (req, res) => {
     try {
-       
-        const data = await Employee.find({ age:{$eq:25}}).explain("executionStats")
+        const data = await Employee.find({ age:{$eq:24}}).explain("executionStats")
         // const data = await Employee.find({ name:"madhu"}).explain("executionStats")
         res.status(200).send({data});
     } 
@@ -32,7 +31,7 @@ Router.get("/indexingstats", async (req, res) => {
 });
 
 Router.get("/removeindexing",async(req,res)=>{
-    await Employee.collection.dropIndex({ age: 1 });
+    await Employee.collection.dropIndex({ age: 1,name:1});
     res.status(200).send("Index deleted successfully!");
 })
 
@@ -40,6 +39,69 @@ Router.get("/getindexes",async(req,res)=>{
     const data=await Employee.collection.getIndexes()
     res.status(200).send({data:data})
 })
+
+
+// part 2
+// Types of Indexing 
+//   1.Single Fields Indexing
+//   2.Compound Indexes (order matters)
+//   3.Text Indexes 
+
+// Bonus: 
+//     Partially Filter Indexing
+//     Covered Query
+
+// ** When not to use indexing
+
+// *1. When The collection is small
+
+// *2. When the collection is frequently updated
+
+// *3. when the queries from multiple fields 
+
+// *4. When collection large make less indexes
+
+
+Router.get("/Compoundindexing", async (req, res) => {
+    try {
+        await Employee.collection.createIndex({ "age": 1,"name":1 });
+        // console.log(await Employee.find({ age:{$gte:12}}).explain("executionStats"));
+        res.status(200).send("Index created successfully!");
+    } 
+    catch (error) {
+        console.error("Error creating index:", error);
+        res.status(500).send("Error creating index");
+    }
+});
+
+Router.get("/ordermatters",async(req,res)=>{
+    // const data=await Employee.find({age:{$gte:25}}).explain("executionStats")
+    // const data=await Employee.find({age:{$gte:25},name:"madhu"}).explain("executionStats")
+    // const data=await Employee.find({name:"madhu",age:{$gte:25}}).explain("executionStats")
+    const data=await Employee.find({age:{$gte:25}})
+    res.status(200).send({data:data})
+})
+
+
+
+Router.get("/partialfilterindexing",async(req,res)=>{
+    try {
+    const data = await Employee.collection.createIndex({age:1},{partialFilterExpression:{age:{$lt:25}}})
+    res.status(200).send("Index created successfully!");
+} 
+catch (error) {
+    console.error("Error creating index:", error);
+    res.status(500).send("Error creating index");
+} 
+})
+
+Router.get("/coverredquery",async(req,res)=>{
+    const data=await Employee.find({age:24}).explain("executionStats")
+    res.status(200).send({data:data})
+})
+
+
+
 
 
 
