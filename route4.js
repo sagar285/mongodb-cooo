ec0317566513521284b487eb2ecbffadd23df6e5
sagar <96648429+sagar285@users.noreply.github.com>
2024-08-30 23:12:49 +0530

    indexing part 3

--- route4.js
+++ route4.js
@@ -101,6 +101,93 @@ Router.get("/coverredquery",async(req,res)=>{
 })
 
 
+// TODO:PART 3 -
+// * Winning plan in mongodb
+//*  what is multi key indexing
+// * Text Indexing (one on per collection)
+
+
+// *************************************
+
+// ***** winning plan calculation*****
+
+// ** 1. mongodb check performance of index on a sample
+// **    of documents once the queries run and set it as winning place.
+// **   it store that winning plan in cache
+
+// TODO: when cache of winning plan reset
+//  **   After 1000 writes operation
+// **  Index is reset
+// **  Mongo server is restarted 
+//**   other indexes are manipulated or updated
+
+
+// TODO:MULTI KEY INDEX
+
+// ** multi key index that can be created on an array field
+
+Router.get("/multipleindexing", async (req, res) => {
+    try {
+        await Employee.collection.createIndex({ age: 1 });
+        await Employee.collection.createIndex({ "age": 1, "name":1 });
+        // console.log(await Employee.find({ age:{$gte:12}}).explain("executionStats"));
+        res.status(200).send("Index created successfully!");
+    } 
+    catch (error) {
+        console.error("Error creating index:", error);
+        res.status(500).send("Error creating index");
+    }
+});
+
+Router.get("/winningplan",async(req,res)=>{
+    const data=await Employee.find({age:24,name:"madhu"}).explain("executionStats")
+    res.status(200).send({data:data})
+})
+
+
+
+Router.get("/multiKeyIndex",async(req,res)=>{
+    try {
+        await Employee.collection.createIndex({ Hobbies: 1 });
+        res.status(200).send("Index created successfully!");
+    } 
+    catch (error) {
+        console.error("Error creating index:", error);
+        res.status(500).send("Error creating index");
+    }
+})
+
+Router.get("/arrayindexing",async(req,res)=>{
+    const data=await Employee.find({Hobbies:"signing"}).explain("executionStats")
+    res.status(200).send({data:data})
+})
+
+
+
+
+
+Router.get("/TextIndexing",async(req,res)=>{
+    try {
+        await Employee.collection.createIndex({bio:"text" });
+        res.status(200).send("Index created successfully!");
+    } 
+    catch (error) {
+        console.error("Error creating index:", error);
+        res.status(500).send("Error creating index");
+    }
+})
+
+Router.get("/searchtextindexing",async(req,res)=>{
+    const data = await Employee.find({$text:{$search:"yotuber"}})
+    res.status(200).send({data:data})
+})
+
+
+
+
+
+
+
 
 
 
